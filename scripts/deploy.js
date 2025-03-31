2f539a28bb156f27158146f737d1e29bcb228595
Akhil1232 <akhiltawde62@gmail.com>
2025-03-31 18:08:12 +0530

    Updated the deploy.js file to add Staking Contract Deployment steps

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,17 @@
 const hre = require("hardhat");
 
 async function main() {
+    //Deploy ERC20 Token
     const DEVECC20 = await hre.ethers.getContractFactory("DEVECC20");
     const devEcc20 = await DEVECC20.deploy();
     await devEcc20.waitForDeployment();
     console.log("DEVECC20 is deployed to:", devEcc20.target);
+
+    //Deploy Staking Contract
+    const Staking = await hre.ethers.getContractFactory("Staking");
+    const staking = await Staking.deploy(devEcc20.target);
+    await staking.waitForDeployment();
+    console.log("Staking contract is deployed at:", staking.target);
 }
 
 main().catch((error) => {
