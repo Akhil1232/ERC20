38511a62ad9eca4ef85128d3af16f9ce443655ec
Akhil1232 <akhiltawde62@gmail.com>
2025-03-31 19:26:29 +0530

    Added hardhat-verify library for verifying the Deployed Contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+
 module.exports = {
   solidity: "0.8.28",
   networks: {
     SepoliaTestnet: {
       url: "https://sepolia.infura.io/v3/a089b0fa21ff415889b3fd5609c4e2d9",
       accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
     }
   }
 };
