38a6eb2c168fca202f065f0264c40ae6da7df80c
Akhil1232 <akhiltawde62@gmail.com>
2025-03-31 16:10:17 +0530

    Updated contract with necessary changes for check: whenNotPaused

--- contracts/token/DEVECC20.sol
+++ contracts/token/DEVECC20.sol
@@ -1,17 +1,19 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract DEVECC20 is ERC20, AccessControl {
+contract DEVECC20 is ERC20Pausable, AccessControl {
 
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
     constructor() ERC20("DEVECC20", "DEV20"){
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
+
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 
@@ -23,16 +25,12 @@ contract DEVECC20 is ERC20, AccessControl {
         _burn(msg.sender, amount);
     }
 
-    function pause() public onlyOwner {
+    function pause() public onlyRole(PAUSER_ROLE){
         _pause();
     }
 
-    function unpause() public onlyOwner {
+    function unpause() public onlyRole(PAUSER_ROLE) {
         _unpause();
-    }
-
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override whenNotPaused {
-        super._beforeTokenTransfer(from, to, amount);
     }
 }
 
